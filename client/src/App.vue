7ac5c951760e7afe364955149268116123e960e5
Anthony Apodaca <anthony.r.apodaca@gmail.com>
2020-08-14 12:15:41 -0600

    nav component vuesax

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/new">New</router-link></li>
-      </ul>
-    </div>
+    <Nav />
     <main>
       <router-view />
     </main>
   </div>
 </template>
 
+<script>
+import Nav from './components/layout/Nav'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
 <style>
 body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-#nav {
-  max-height: 20vh;
-  min-width: 100vw;
-  background-color: #f4f4f4;
-  -webkit-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
-  -moz-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
-  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
-}
-
-#nav ul {
-  display: flex;
-}
-
-#nav li {
-  min-height: 24px;
 }
 </style>
---
+++ client/src/components/layout/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <vs-navbar collapse v-model="activeItem" class="nabarx">
+      <div slot="title">
+        <vs-navbar-title>
+          myAF
+        </vs-navbar-title>
+      </div>
+
+      <vs-navbar-item >
+        <router-link to="/">Home</router-link>
+      </vs-navbar-item>
+       <vs-navbar-item >
+        <router-link to="/new">New</router-link>
+      </vs-navbar-item>
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style scoped></style>
